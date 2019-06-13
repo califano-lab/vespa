07c0948db6c88f12c9bab7148637ffa203e49cac
George Rosenberger <gr2578@cumc.columbia.edu>
2019-06-13 08:57:09 -0400

    [FEATURE] More adapters

--- R/aracne.R
+++ R/aracne.R
@@ -6,20 +6,19 @@
 #' @param output_dir Output directory
 #' @param kinases List of kinases (UniProtKB)
 #' @param phosphatases List of phosphatases (UniProtKB)
-#' @return peptide-level phosphoviper data.table
 #' @import data.table
 #' @export
-exportARACNe<-function(datl, output_dir, kinases, phosphatases) {
+export2hparacne<-function(datl, output_dir, kinases, phosphatases) {
   # format output directory
   dir.create(output_dir)
 
   # reduce data to top peptide query per phosphosite
   pqp_freq<-data.table(merge(unique(datl[,c("site_id","peptide_id")]), as.data.frame(table(datl$peptide_id),stringsAsFactors = FALSE), by.x="peptide_id", by.y="Var1"))
-  pqp_top<-pqp_freq[pqp_freq[, .I[which.max(Freq)], by=site_id]$V1]$peptide_id
-  datl<-subset(datl, peptide_id %in% pqp_top)
+  pqp_top<-pqp_freq[pqp_freq[, .I[which.max(Freq)], by=site_id]$V1]
+  datl<-merge(datl, pqp_top[,c("site_id","peptide_id"),which=FALSE], by=c("site_id","peptide_id"))
 
   # generate matrix
-  datm<-dcast(unique(datl[,c("site_id","run_id","peptide_intensity")]), site_id ~ run_id, value.var = "peptide_intensity", fun.aggregate=mean)
+  datm<-dcast(unique(datl[,c("peptide_id","run_id","peptide_intensity")]), peptide_id ~ run_id, value.var = "peptide_intensity")
 
   # get annotation
   phosphoAnno<-as.matrix(datm[,1])
@@ -33,8 +32,123 @@ exportARACNe<-function(datl, output_dir, kinases, phosphatases) {
   write.table(unique(datl[,c("gene_id","protein_id","peptide_id","site_id","modified_peptide_sequence","peptide_sequence","phosphosite")]), file=file.path(output_dir,"peptides.txt"), quote=FALSE, row.names=FALSE, sep="\t")
 
   # write kinases
-  write.table(unique(subset(datl, protein_id %in% kinases)$site_id), file=file.path(output_dir,"kinases.txt"), quote=FALSE, col.names=FALSE, row.names=FALSE, sep="\t")
+  write.table(unique(subset(datl, protein_id %in% kinases)$peptide_id), file=file.path(output_dir,"kinases.txt"), quote=FALSE, col.names=FALSE, row.names=FALSE, sep="\t")
 
   # write kinases + phosphatases
-  write.table(unique(c(subset(datl, protein_id %in% kinases)$site_id, subset(datl, protein_id %in% phosphatases)$site_id)), file=file.path(output_dir,"kinases_phosphatases.txt"), quote=FALSE, col.names=FALSE, row.names=FALSE, sep="\t")
+  write.table(unique(c(subset(datl, protein_id %in% kinases)$peptide_id, subset(datl, protein_id %in% phosphatases)$peptide_id)), file=file.path(output_dir,"kinases_phosphatases.txt"), quote=FALSE, col.names=FALSE, row.names=FALSE, sep="\t")
+}
+
+#' Export to matrix
+#'
+#' This function exports tables in the unified phosphoviper format to a matrix
+#'
+#' @param datl Unified phosphoviper format
+#' @return matrix
+#' @import data.table
+#' @export
+export2mx<-function(datl) {
+  # reduce data to top peptide query per phosphosite
+  pqp_freq<-data.table(merge(unique(datl[,c("site_id","peptide_id")]), as.data.frame(table(datl$peptide_id),stringsAsFactors = FALSE), by.x="peptide_id", by.y="Var1"))
+  pqp_top<-pqp_freq[pqp_freq[, .I[which.max(Freq)], by=site_id]$V1]
+  datl<-merge(datl, pqp_top[,c("site_id","peptide_id"),which=FALSE], by=c("site_id","peptide_id"))
+
+  # generate matrix
+  datm<-dcast(unique(datl[,c("site_id","run_id","peptide_intensity")]), site_id ~ run_id, value.var = "peptide_intensity")
+
+  # get annotation
+  phosphoExp<-as.matrix(datm[,-1])
+  phosphoExp[phosphoExp == 0]<-NA
+
+  rownames(phosphoExp)<-datm$site_id
+
+  return(phosphoExp)
+}
+
+#' Import hpARACNe network
+#'
+#' This function imports a hpARACNe network and generates site-specific regulons
+#'
+#' @param afile hpARACNe network file
+#' @param pfile hpARACNe peptides file
+#' @import data.table
+#' @importFrom plyr dlply .
+#' @export
+hparacne2regulon<-function(afile, pfile) {
+  aracne<-fread(afile)
+  peptides<-fread(pfile)
+
+  # Map peptide identifiers to site identififers
+  aracne<-merge(aracne,peptides[,c("peptide_id","site_id"), with = FALSE], by.x="Regulator", by.y="peptide_id", allow.cartesian=TRUE)
+  aracne<-merge(aracne,peptides[,c("peptide_id","site_id"), with = FALSE], by.x="Target", by.y="peptide_id", allow.cartesian=TRUE)
+  aracne<-aracne[,c("site_id.x","site_id.y","MI","Correlation"), with = FALSE]
+  names(aracne)<-c("Regulator","Target","MI","Correlation")
+
+  # Compute likelihood from MI
+  aracne$likelihood<-(aracne$MI / max(aracne$MI))
+
+  # Generate regulons
+  regulons<-dlply(aracne,.(Regulator),function(X){tfmode<-X$Correlation;names(tfmode)<-X$Target;return(list("tfmode"=tfmode, "likelihood"=X$likelihood))})
+
+  return(regulons)
+}
+
+#' Convert site-specific regulons to protein regulons
+#'
+#' This function converts site-specific regulons to a list of protein - protein site-specific regulons
+#'
+#' @param regulons VIPER regulons
+#' @import data.table
+#' @importFrom plyr dlply .
+#' @export
+site2protein<-function(regulons) {
+  # Generate regulon map
+  regulon_map<-data.table("site_id"=names(regulons),"protein_id"=as.vector(sapply(names(regulons),function(X){strsplit(X,":")[[1]][2]})))
+
+  # For each protein site, generate a unique regulon
+  regulon_map[,regulon_id:=c(1:length(unique(site_id))),key="protein_id"]
+
+  # Generate list of regulons
+  regulons_list<-dlply(regulon_map,.(regulon_id),function(X){subregulons<-regulons[X$site_id];names(subregulons)<-X$protein_id;return(subregulons)})
+
+  return(regulons_list)
+}
+
+#' Convert site-specific regulons to gene regulons
+#'
+#' This function converts site-specific regulons to a list of gene - gene regulons
+#'
+#' @param regulons VIPER regulons
+#' @import data.table
+#' @importFrom plyr dlply .
+#' @export
+site2gene<-function(regulons) {
+  # Generate site_id map
+  site_ids<-unique(c(names(regulons),unlist(sapply(regulons,function(X){names(X$tfmode)}))))
+  id_map<-data.table("site_id"=site_ids,"gene_id"=as.vector(sapply(ids,function(X){strsplit(X,":")[[1]][1]})))
+
+  # Generate regulon map
+  regulon_map<-data.table("site_id"=names(regulons),"gene_id"=as.vector(sapply(names(regulons),function(X){strsplit(X,":")[[1]][1]})))
+
+  # For each protein site, generate a unique regulon
+  regulon_map[,regulon_id:=c(1:length(unique(site_id))),key="gene_id"]
+
+  # Generate list of regulons
+  regulons_list<-dlply(regulon_map,.(regulon_id),function(X){subregulons<-regulons[X$site_id];names(subregulons)<-X$gene_id;subregulons<-lapply(subregulons,function(Y){ids<-names(Y$tfmode);names(Y$tfmode)<-with(id_map, gene_id[match(ids,site_id)]);return(Y)});return(subregulons)})
+
+  regulons_list_averaged<-lapply(regulons_list,function(X){lapply(X,function(Y){res<-sapply(unique(names(Y$tfmode)),function(Z){idx<-which(names(Y$tfmode)==Z);return(list("tfmode"=mean(Y$tfmode[idx]),"likelihood"=mean(Y$likelihood[idx])))});return(list("tfmode"=unlist(res[1,]),"likelihood"=unname(unlist(res[2,]))))})})
+
+  return(regulons_list_averaged)
+}
+
+#' Subset regulons for targets
+#'
+#' This function subsets regulons to specified targets only
+#'
+#' @param regulons VIPER regulons
+#' @param target Vector of targets
+#' @export
+subset_regulon<-function(regulons,targets) {
+  subregulon<-lapply(regulons,function(X){ids<-which(names(X$tfmode) %in% targets);if(length(ids)>0){return(list("tfmode"=X$tfmode[ids], "likelihood"=X$likelihood[ids]))}})
+
+  return(subregulon[sapply(subregulon,length)>0])
 }
--- R/import.R
+++ R/import.R
@@ -1,3 +1,105 @@
+replace_ptms<-function(X){
+  X<-str_replace_all(X,"S\\(Phospho\\)","s")
+  X<-str_replace_all(X,"T\\(Phospho\\)","t")
+  X<-str_replace_all(X,"Y\\(Phospho\\)","y")
+  X<-str_replace_all(X,"\\.","")
+  X<-gsub("\\s*\\([^\\)]+\\)","",as.character(X))
+  return(X)
+}
+
+strip_ptms<-function(X){
+  X<-str_replace_all(X,"\\.","")
+  X<-gsub("\\s*\\([^\\)]+\\)","",as.character(X))
+  return(X)
+}
+
+convert_sites<-function(fasta, gene_id, protein_id, modified_peptide_sequence){
+  peptide_start<-str_locate(fasta[protein_id], str_to_upper(modified_peptide_sequence))[1]
+  peptide_sites<-str_locate_all(modified_peptide_sequence, c("s","t","y"))
+
+  s_sites<-peptide_sites[[1]][,1] + (peptide_start - 1)
+  t_sites<-peptide_sites[[2]][,1] + (peptide_start - 1)
+  y_sites<-peptide_sites[[3]][,1] + (peptide_start - 1)
+
+  protein_sites<-rbind(
+    data.frame("aa"=rep("S", length(s_sites)), "site"=s_sites, stringsAsFactors = FALSE),
+    data.frame("aa"=rep("T", length(t_sites)), "site"=t_sites, stringsAsFactors = FALSE),
+    data.frame("aa"=rep("Y", length(y_sites)), "site"=y_sites, stringsAsFactors = FALSE)
+    , make.row.names = FALSE)
+
+  protein_sites$phosphosite<-paste(protein_sites$aa,protein_sites$site,sep="")
+  protein_sites$site_id<-paste(gene_id,protein_id,protein_sites$phosphosite,sep=":")
+
+  return(protein_sites[,c("site_id","phosphosite")])
+}
+
+#' Import OpenSWATH TSV file
+#'
+#' This function imports a OpenSWATH TSV file and converts the data to the unified phosphoviper format
+#'
+#' @param file OpenSWATH TSV file
+#' @param fasta Amino acid FASTA file from UniProt
+#' @param cores Integer indicating the number of cores to use (only 1 in Windows-based systems)
+#' @return peptide-level phosphoviper data.table
+#' @import data.table
+#' @importFrom plyr ddply .
+#' @import seqinr
+#' @import stringr
+#' @import pbapply
+#' @import preprocessCore
+#' @export
+importOpenSWATH<-function(file, fasta, cores = 1) {
+
+  # load reference fasta library
+  message("Loading FASTA DB")
+  fasta<-read.fasta(fasta, seqtype="AA", as.string = TRUE, set.attributes = FALSE)
+  genes_proteins<-data.table("gene_id"=as.vector(sapply(names(fasta),function(X){strsplit(strsplit(X,"\\|")[[1]][3],"_")[[1]][1]})), "protein_id"=as.vector(sapply(names(fasta),function(X){strsplit(X,"\\|")[[1]][2]})))
+  names(fasta)<-as.vector(sapply(names(fasta),function(X){strsplit(X,"\\|")[[1]][2]}))
+
+  # load OpenSWATH data
+  message("Loading OpenSWATH data")
+  dat<-fread(file)
+  dat<-dat[,c("filename","Sequence","FullPeptideName","transition_group_id","Intensity"),with = FALSE]
+  names(dat)<-c("run_id","peptide_sequence","modified_peptide_sequence","peptide_id","peptide_intensity")
+
+  # map peptides to proteins
+  dat$modified_peptide_sequence<-strip_ptms(replace_ptms(dat$modified_peptide_sequence))
+  peptides_proteins<-data.table("peptide_sequence"=unique(dat$peptide_sequence))
+  message("Mapping OpenSWATH peptides to FASTA")
+  peptides_proteins$protein_id<-names(fasta)[pbsapply(peptides_proteins$peptide_sequence,function(X){index<-str_which(fasta,X);if(length(index)==1){return(index)}else{return(length(fasta)+1)}}, cl=cores)]
+  datl<-merge(merge(peptides_proteins, dat, by="peptide_sequence"), genes_proteins, by="protein_id")
+
+  # modify run identifier
+  datl$run_id<-as.vector(sapply(datl$run_id,function(X){strsplit(X,"\\/\\/")[[1]][2]}))
+  datl$run_id<-as.vector(sapply(datl$run_id,function(X){strsplit(X,"\\.")[[1]][1]}))
+
+  # map phosphosites
+  message("Mapping phosphopeptide sites")
+  site_mapping<-as.data.table(ddply(unique(datl[,c("gene_id","protein_id","modified_peptide_sequence")]),.(modified_peptide_sequence),function(X){convert_sites(fasta,X$gene_id,X$protein_id,X$modified_peptide_sequence)}))
+  site_mapping<-merge(site_mapping, unique(datl[,c("gene_id","protein_id","peptide_id","peptide_sequence","modified_peptide_sequence")]),by="modified_peptide_sequence")
+
+  # generate matrix
+  datmx<-dcast(unique(datl[,c("peptide_id","run_id","peptide_intensity")]), peptide_id ~ run_id, value.var = "peptide_intensity")
+
+  # get annotation
+  rowids<-datmx$peptide_id
+  colids<-colnames(datmx[,-1])
+  datmx<-as.matrix(datmx[,-1])
+  datmx[datmx == 0]<-NA
+
+  # normalize matrix
+  datmxn<-log10(normalize.quantiles(datmx))
+  colnames(datmxn)<-colids
+  datmxn<-data.table(datmxn)
+  datmxn$peptide_id<-rowids
+
+  datln<-melt(datmxn, id.vars="peptide_id", variable.name="run_id", value.name="peptide_intensity")
+
+  datl<-merge(datln, site_mapping,by="peptide_id", allow.cartesian=TRUE)
+
+  return(datl[,c("gene_id","protein_id","peptide_id","site_id","modified_peptide_sequence","peptide_sequence","phosphosite","run_id","peptide_intensity")])
+}
+
 #' Import CPTAC file
 #'
 #' This function imports a CPTAC file and converts the data to the unified phosphoviper format
@@ -13,26 +115,6 @@
 #' @import pbapply
 #' @export
 importCPTAC<-function(file, fasta, cores = 1) {
-  convert_sites<-function(fasta, gene_id, protein_id, modified_peptide_sequence){
-    peptide_start<-str_locate(fasta[protein_id], str_to_upper(modified_peptide_sequence))[1]
-    peptide_sites<-str_locate_all(modified_peptide_sequence, c("s","t","y"))
-
-    s_sites<-peptide_sites[[1]][,1] + (peptide_start - 1)
-    t_sites<-peptide_sites[[2]][,1] + (peptide_start - 1)
-    y_sites<-peptide_sites[[3]][,1] + (peptide_start - 1)
-
-    protein_sites<-rbind(
-      data.frame("aa"=rep("S", length(s_sites)), "site"=s_sites, stringsAsFactors = FALSE),
-      data.frame("aa"=rep("T", length(t_sites)), "site"=t_sites, stringsAsFactors = FALSE),
-      data.frame("aa"=rep("Y", length(y_sites)), "site"=y_sites, stringsAsFactors = FALSE)
-      , make.row.names = FALSE)
-
-    protein_sites$phosphosite<-paste(protein_sites$aa,protein_sites$site,sep="")
-    protein_sites$site_id<-paste(gene_id,protein_id,protein_sites$phosphosite,sep=":")
-
-    return(protein_sites[,c("site_id","phosphosite")])
-  }
-
   # load reference fasta library
   message("Loading FASTA DB")
   fasta<-read.fasta(fasta, seqtype="AA", as.string = TRUE, set.attributes = FALSE)
@@ -64,6 +146,35 @@ importCPTAC<-function(file, fasta, cores = 1) {
   site_mapping<-as.data.table(ddply(unique(datl[,c("gene_id","protein_id","modified_peptide_sequence")]),.(modified_peptide_sequence),function(X){convert_sites(fasta,X$gene_id,X$protein_id,X$modified_peptide_sequence)}))
 
   datl<-merge(datl,site_mapping[,c("modified_peptide_sequence","phosphosite","site_id")],by="modified_peptide_sequence", allow.cartesian=TRUE)
+
+  return(datl[,c("gene_id","protein_id","peptide_id","site_id","modified_peptide_sequence","peptide_sequence","phosphosite","run_id","peptide_intensity")])
+}
+
+#' Import CCT file
+#'
+#' This function imports a CCT file and converts the data to the unified phosphoviper format
+#'
+#' @param file CPTAC file
+#' @return peptide-level phosphoviper data.table
+#' @import data.table
+#' @importFrom plyr ddply .
+#' @export
+importCCT<-function(file) {
+  # load CCT data
+  message("Loading CCT data")
+  dat<-fread(file)
+  dat$gene_id<-sapply(dat$V1,function(X){strsplit(X,"_")[[1]][1]})
+  dat$protein_id<-sapply(sapply(dat$V1,function(X){strsplit(X,"__")[[1]][2]}),function(X){strsplit(X,":")[[1]][1]})
+  dat$phosphosite<-sapply(sapply(dat$V1,function(X){strsplit(X,"__")[[1]][2]}),function(X){strsplit(X,":")[[1]][2]})
+  dat$peptide_id<-dat$V1
+  dat$peptide_sequence<-dat$V1
+  dat$modified_peptide_sequence<-dat$V1
+  dat[,V1:=NULL]
+  dat$site_id<-paste(dat$gene_id,dat$protein_id,dat$phosphosite,sep=":")
+
+  # transform data to list
+  datl<-melt(dat, id.vars=c("gene_id","protein_id","site_id","peptide_id","peptide_sequence","modified_peptide_sequence","phosphosite"), variable.name="run_id", value.name="peptide_intensity")
+  datl<-datl[complete.cases(datl),]
 
   return(datl[,c("gene_id","protein_id","peptide_id","site_id","modified_peptide_sequence","peptide_sequence","phosphosite","run_id","peptide_intensity")])
 }
